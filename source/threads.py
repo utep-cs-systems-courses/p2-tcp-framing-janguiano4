e5389b56bd0c375084ba66241e90f5f57894c6b7
Joel Anguiano <janguiano4@miners.utep.edu>
2021-03-31 23:37:30 -0600

    Part 2

---
+++ source/threads.py
@@ -0,0 +1,29 @@
+import socket, sys, re, time, os
+sys.path.append("../lib")
+import params
+from framedSocket import framedSocket
+import threader
+
+switchesVarDefaults = (
+    (('-l','--listenPort'), 'listenPort', 50001),
+    (('-?','--usage'),"usage",False),
+    )
+
+progname = "echoserver"
+paramMap = paramMap = params.parseParams(switchesVarDefaults)
+
+listenPort = paramMap['listenPort']
+listenAddr = ''
+
+if paramMap['usage']:
+    params.usage()
+
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((listenAddr,listenPort))
+s.listen(1)
+
+
+while True:
+    conn,addr = s.accept()
+    threader.Worker(conn,addr).start()
