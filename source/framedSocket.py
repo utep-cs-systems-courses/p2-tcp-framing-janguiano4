4cd87b38d2a688aa2c4647bff11a7f258b0d1a68
Joel Anguiano <janguiano4@miners.utep.edu>
2021-04-02 08:45:49 -0600

    Comments added

--- source/framedSocket.py
+++ source/framedSocket.py
@@ -1,21 +1,24 @@
+#Joel Anguiano
+#file contains framedSocket class
+
 class framedSocket:
-    def _init_(self,connectedSocket):
-        self.cs = connectedSocket
+    def _init_(self,connectedSocket): #This is how make an instance of the class, pass connSocket
+        self.cs = connectedSocket         #this is how send messages between the client and server 
 
     def sendMessage(self,message):
-        lengthStr = str(len(message)) + ':'
-        lengthBA = bytearray(lengthStr,'utf-8')
-        message = lengthBA + message
-        self.cs.send(message)
+        lengthStr = str(len(message)) + ':' #takes mess. converts to lenght# then to string
+        lengthBA = bytearray(lengthStr,'utf-8') #converts lenghtStr in bytearray
+        message = lengthBA + message #Concatanates bytearray with origal message
+        self.cs.send(message) #sends mess. to connected socket
 
     def receiveMessage(self):
-        msgList = []
-        data = self.cs.recv(100).decode()
+        msgList = [] #mess. is empty
+        data = self.cs.recv(100).decode() #this is what were going to read from the sockete
         left,right = partition(data)
-        msgList.append(data[left:right])
-        data = data[right:]
+        msgList.append(data[left:right]) #fill data from left to right
+        data = data[right:] #removes what we already read
 
-        while(data):
+        while(data): #keep iterating while there is still data
             left,right = partition(data)
             if len(data) < right:
                 data += self.cs.recv(100).decode()
@@ -24,7 +27,7 @@ class framedSocket:
                 data = data[right:]
         return msgList
 
-def partition(string):
+def partition(string): #method indicates first and last character in a message
     num = ""
     while(string[0].isdigit()):
         num += string[0]
